1b5e5f2f3913f1ef3f942eee32c2963be659697d
Jesus Borjas <jesusbor006@gmail.com>
2023-05-18 21:56:48 -0500

    Add filter by category functionality

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,12 +4,37 @@ import itemsData from "../../data";
 import ItemList from "../ItemList/ItemList";
 
 function App() {
-  const [items, setItems] = useState(itemsData);
-  console.log(setItems);
+  const [allItems, setAllItems] = useState(itemsData);
+  const [filteredItems, setFilteredItems] = useState(allItems);
+
+  const getFilteredItems = (type) => {
+    const filtered = allItems.filter((item) => {
+      return item.category === type;
+    });
+    setFilteredItems(filtered);
+  };
 
   return (
     <main className="main">
-      <ItemList items={items} />
+      <button
+        onClick={() => getFilteredItems("technology")}
+        className="category-btn"
+      >
+        Technology
+      </button>
+      <button
+        onClick={() => getFilteredItems("pet supplies")}
+        className="category-btn"
+      >
+        Pet Supplies
+      </button>
+      <button
+        onClick={() => getFilteredItems("personal care")}
+        className="category-btn"
+      >
+        Personal Care
+      </button>
+      <ItemList items={filteredItems} />
     </main>
   );
 }
