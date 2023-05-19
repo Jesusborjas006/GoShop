ccc38581bdb684586d44e17e5cf8b8f8d4e33348
Jesus Borjas <jesusbor006@gmail.com>
2023-05-18 20:24:03 -0500

    Add file to keep data for items

---
+++ src/data.js
@@ -0,0 +1,58 @@
+const items = [
+  {
+    id: 1,
+    name: "AirPods",
+    category: "technology",
+    price: 100,
+  },
+  {
+    id: 2,
+    name: "Iphone 14 Pro",
+    category: "technology",
+    price: 999,
+  },
+  {
+    id: 3,
+    name: "Samsung TV",
+    category: "technology",
+    price: 2000,
+  },
+  {
+    id: 4,
+    name: "Dog Food",
+    category: "pet supplies",
+    price: 20,
+  },
+  {
+    id: 5,
+    name: "Dog Leash",
+    category: "pet supplies",
+    price: 10,
+  },
+  {
+    id: 6,
+    name: "Catnip",
+    category: "pet supplies",
+    price: 3,
+  },
+  {
+    id: 7,
+    name: "Mighty Patch",
+    category: "personal care",
+    price: 3,
+  },
+  {
+    id: 8,
+    name: "CeraVe",
+    category: "personal care",
+    price: 15,
+  },
+  {
+    id: 9,
+    name: "Cologne",
+    category: "personal care",
+    price: 80,
+  },
+];
+
+export default items;
