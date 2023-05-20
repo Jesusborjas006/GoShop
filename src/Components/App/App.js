53e6f2e70b1e38eddd308cf95ca6bd040c504d4c
Jesus Borjas <jesusbor006@gmail.com>
2023-05-19 21:05:46 -0500

    Remove all eslint errors

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,34 +6,44 @@ import ItemList from "../ItemList/ItemList";
 function App() {
   const [allItems, setAllItems] = useState(itemsData);
   const [filteredItems, setFilteredItems] = useState(allItems);
+  console.log(setAllItems)
 
   const getFilteredItems = (type) => {
-    const filtered = allItems.filter((item) => {
-      return item.category === type;
-    });
+    const filtered = allItems.filter((item) => item.category === type);
     setFilteredItems(filtered);
   };
 
   return (
     <main className="main">
-      <button
-        onClick={() => getFilteredItems("technology")}
-        className="category-btn"
-      >
-        Technology
-      </button>
-      <button
-        onClick={() => getFilteredItems("pet supplies")}
-        className="category-btn"
-      >
-        Pet Supplies
-      </button>
-      <button
-        onClick={() => getFilteredItems("personal care")}
-        className="category-btn"
-      >
-        Personal Care
-      </button>
+      <div className="filter-btn-container">
+        <button
+          onClick={() => getFilteredItems("technology")}
+          className="category-btn"
+        >
+          Technology
+        </button>
+        <button
+          onClick={() => getFilteredItems("pet supplies")}
+          className="category-btn"
+        >
+          Pet Supplies
+        </button>
+        <button
+          onClick={() => getFilteredItems("personal care")}
+          className="category-btn"
+        >
+          Personal Care
+        </button>
+        {filteredItems.length <= 3 && (
+          <button
+            onClick={() => setFilteredItems(allItems)}
+            className="category-btn"
+          >
+            Show All
+          </button>
+        )}
+      </div>
+
       <ItemList items={filteredItems} />
     </main>
   );
