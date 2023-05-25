1983c51e8bbb56c3a46cb6b53622608cb3d8d737
Jesus Borjas <jesusbor006@gmail.com>
2023-05-24 21:29:30 -0500

    Add router to navigate between cart page and home

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,10 +4,11 @@ import itemsData from "../../data";
 import ItemList from "../ItemList/ItemList";
 import Navbar from "../Navbar/Navbar";
 import Category from "../Category/Category";
+import { Route } from "react-router-dom";
 
 function App() {
   const [allItems, setAllItems] = useState(itemsData);
-  console.log(setAllItems)
+  console.log(setAllItems);
   const [filteredItems, setFilteredItems] = useState(allItems);
 
   const getFilteredItems = (type) => {
@@ -19,21 +20,27 @@ function App() {
     <main className="main">
       <Navbar />
 
-      <div className="main-content">
-        <Category
-          filterFunc={getFilteredItems}
-          allItems={allItems}
-          setFilteredItems={setFilteredItems}
-        />
-        <div className="right-content">
-          <h3 className="total-products">
-            {filteredItems.length === 1
-              ? "1 product"
-              : `${filteredItems.length} total products`}
-          </h3>
-          <ItemList items={filteredItems} />
+      <Route exact path="/">
+        <div className="main-content">
+          <Category
+            filterFunc={getFilteredItems}
+            allItems={allItems}
+            setFilteredItems={setFilteredItems}
+          />
+          <div className="right-content">
+            <h3 className="total-products">
+              {filteredItems.length === 1
+                ? "1 product"
+                : `${filteredItems.length} total products`}
+            </h3>
+            <ItemList items={filteredItems} />
+          </div>
         </div>
-      </div>
+      </Route>
+
+      <Route exact path="/cart">
+        <h1>Cart</h1>
+      </Route>
     </main>
   );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
 import "../App/App.css";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1 className="logo">GoShop</h1>
-      <p className="cart">Cart</p>
+      <Link to="/">
+        <h1 className="logo">GoShop</h1>
+      </Link>
+      <Link to="/cart">
+        <p className="cart">Cart</p>
+      </Link>
     </nav>
   );
 };
